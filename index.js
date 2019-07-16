ca02d94bd0658e293b7767960010f1a109b6847c
Mauricio Mercado <mau@maumercado.com>
2019-07-15 17:53:07 -0700

    fix(index): added backward compatibility

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const { safeLoad } = require('js-yaml')
 const Ajv = require('ajv')
 const ColophonError = require('./error')
-
-const { latest: { schema } } = require('./versions')
+const versions = require('./versions')
 
 // force async
-schema['$async'] = true
 
 const ajv = new Ajv({ allErrors: true })
 
-const validate = ajv.compile(schema)
-
 module.exports = function parser (data) {
   // attempt to read yaml
+  if (!data) {
+    return Promise.reject(new Error('Received invalid data'))
+  }
+
   if (typeof data === 'string') {
     try {
       data = safeLoad(data)
@@ -21,6 +21,12 @@ module.exports = function parser (data) {
       return Promise.reject(error)
     }
   }
+
+  // load latest and prev to latest version
+  // for backwards compatibility
+  const { schema } = versions[parseFloat(data.version).toFixed(1)]
+  schema['$async'] = true
+  const validate = ajv.compile(schema)
 
   // validate
   return validate(data)
--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ const { test } = require('tap')
 const parser = require('..')
 
 const yaml = readFileSync(join(__dirname, '3.0', 'fixtures', '.colophon.yml'), 'utf8')
+const yamlv2 = readFileSync(join(__dirname, '2.0', 'fixtures', '.colophon.yml'), 'utf8')
 
 test('valid colophon file', assert => {
   assert.plan(1)
@@ -34,4 +35,12 @@ test('provides schema errors', assert => {
   `
 
   assert.rejects(parser(colophon), { errors: [ { message: "should have required property 'contacts'" } ] })
+})
+
+test('valid colophon file with backwards compatibility', assert => {
+  assert.plan(1)
+
+  const data = safeLoad(yamlv2)
+
+  assert.resolveMatch(parser(data), { version: 2.0, id: 'awesome-app' })
 })
