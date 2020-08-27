d3c23a1dbcabc7563a85039d58db4edd3c088505
Phil Dufault <phil@dufault.info>
2020-08-27 16:12:15 -0700

    feat: Add colophon-validator command (#10)
    * feat: add colophon-validator command, deprecates need for colophon-validator repo
    
    * chore: align with linting standards
    
    * chore: remove npm release flow from github actions

---
+++ validate.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+
+const parser = require('./index.js')
+const fs = require('fs')
+
+const { argv } = require('yargs')
+  .usage('Usage: $0 <command> [options]')
+  .command('colophon-validator', 'Validate Colophon file schema.')
+  .example('$0 -f .colophon.yml', 'Validate Colophon file schema.')
+  .alias('f', 'file')
+  .nargs('f', 1)
+  .describe('f', 'Load a file')
+  .default('f', '.colophon.yml')
+  .help('h')
+  .alias('h', 'help')
+  .epilog('Copyright 2020')
+
+function loadYaml () {
+  try {
+    return fs.readFileSync(argv.file, 'utf8')
+  } catch (err) {
+    console.error(`"${argv.file}" doesnt exist!`)
+    return process.exit(1)
+  }
+}
+
+async function validateSchema (yaml) {
+  try {
+    await parser(yaml)
+    console.log('Colophon validation successful.')
+  } catch (err) {
+    console.error('Colophon validation failed.\n', err.errors)
+  }
+}
+
+validateSchema(loadYaml())
